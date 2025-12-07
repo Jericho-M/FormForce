3b5e6b345a6177d40ddcfb85fe241ee836f127c5
Jericho-M <jericho.mordasiewicz@formlabs.com>
2025-12-06 23:54:35 -0500

    actual 0 rejection

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ function handleData(event) {
     const dataView = event.target.value;
     const massKg = dataView.getFloat32(0, true); 
 
-    if (massKg < 0) {
+    if (massKg <= 0) {
         massKg = 0;
     }
     
